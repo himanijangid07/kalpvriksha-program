168abe72b4f7dd2d477b9d68eb2e7364a83672ac
Himani Jangid <himanijangid07@gmail.com>
2025-12-21 11:00:29 +0530

    completed

--- Assignment 9/socketIPCMechanism/client/client.c
+++ Assignment 9/socketIPCMechanism/client/client.c
@@ -6,31 +6,41 @@
 #define PORT 8080
 
 int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    while (1) {
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(PORT);
+        addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+        connect(sock, (struct sockaddr *)&addr, sizeof(addr));
 
-    int choice, amount;
-    printf("1. Withdraw\n2. Deposit\n3. Balance\n");
-    scanf("%d", &choice);
+        int choice, amount;
+        char buffer[200];
 
-    write(sock, &choice, sizeof(choice));
+        printf("\n1. Withdraw\n2. Deposit\n3. Balance\n4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
 
-    if(choice == 1 || choice == 2) {
-        scanf("%d", &amount);
-        write(sock, &amount, sizeof(amount));
+        if (choice == 4) {
+            close(sock);
+            break;
+        }
+
+        write(sock, &choice, sizeof(choice));
+
+        if (choice == 1 || choice == 2) {
+            printf("Enter amount: ");
+            scanf("%d", &amount);
+            write(sock, &amount, sizeof(amount));
+        }
+        int bytes = read(sock, buffer, sizeof(buffer) - 1);
+        buffer[bytes] = '\0';
+        printf("Server: %s", buffer);
+
+        close(sock);
     }
-
-    char buffer[100];
-    read(sock, buffer, sizeof(buffer));
-    printf("%s", buffer);
-
-    close(sock);
 
     return 0;
 }
--- Assignment 9/socketIPCMechanism/server/server.c
+++ Assignment 9/socketIPCMechanism/server/server.c
@@ -2,74 +2,88 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+
+#define PORT 8080
+#define DB_FILE "../resource/accountDB.txt"
 
 pthread_mutex_t lock;
-#define PORT 8080
 
-void* handleClient(void* arg) {
-    int sock = *(int*)arg;
+void *handle_client(void *arg) {
+    int client_sock = *(int *)arg;
     free(arg);
 
     int choice, amount;
-    FILE* fp;
+    char response[100];
+    int balance;
+
+    read(client_sock, &choice, sizeof(choice));
 
     pthread_mutex_lock(&lock);
-    fp = fopen("accountDB.txt", "r+");
 
-    int balance;
-    fscanf(fp, "%d", &balance);
-
-    read(sock, &choice, sizeof(choice));
-
-    if(choice == 1) {
-        read(sock, &amount, sizeof(amount));
-
-        if(balance >= amount) {
-            balance -= amount;
-            fprintf(fp, "%d ", balance);
-            write(sock, "Withdraw Success\n", 16);
-        } else {
-            write(sock, "Insufficient Balance\n", 22);
-        }
-    } else if(choice == 2) {
-        read(sock, &amount, sizeof(amount));
-        balance += amount;
-        fprintf(fp, "%d ", balance);
-        write(sock, "Deposit Success\n", 15);
-    } else if(choice == 3) {
-        char msg[50];
-        sprintf(msg, "Balance: %d\n", balance);
-        write(sock, msg, strlen(msg));
+    FILE *fp = fopen(DB_FILE, "r+");
+    if (!fp) {
+        sprintf(response, "File error\n");
+        write(client_sock, response, strlen(response));
+        pthread_mutex_unlock(&lock);
+        close(client_sock);
+        return NULL;
     }
 
+    fscanf(fp, "%d", &balance);
+
+    if (choice == 1) {
+        read(client_sock, &amount, sizeof(amount));
+        if (amount <= balance) {
+            balance -= amount;
+            sprintf(response, "Withdraw %d SUCCESS | Balance = %d\n", amount, balance);
+        } else {
+            sprintf(response, "Withdraw %d FAILED | Balance = %d\n", amount, balance);
+        }
+    } else if (choice == 2) { 
+        read(client_sock, &amount, sizeof(amount));
+        balance += amount;
+        sprintf(response, "Deposit %d SUCCESS | Balance = %d\n", amount, balance);
+    } else if (choice == 3) {
+        sprintf(response, "Current Balance = %d\n", balance);
+    }
+
+    rewind(fp);
+    ftruncate(fileno(fp), 0);
+    fprintf(fp, "%d", balance);
     fclose(fp);
+
     pthread_mutex_unlock(&lock);
-    close(sock);
+
+    write(client_sock, response, strlen(response));
+    close(client_sock);
     return NULL;
 }
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     listen(server_fd, 5);
 
     pthread_mutex_init(&lock, NULL);
 
-    while(1) {
-        int client = accept(server_fd, NULL, NULL);
-        pthread_t tid;
-        int* p = malloc(sizeof(int));
-        *p = client;
+    printf("Server running on port %d...\n", PORT);
+    fflush(stdout);
 
-        pthread_create(&tid, NULL, handleClient, p);
+    while (1) {
+        int *client_sock = malloc(sizeof(int));
+        *client_sock = accept(server_fd, NULL, NULL);
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, handle_client, client_sock);
+        pthread_detach(tid);
     }
 
     return 0;
